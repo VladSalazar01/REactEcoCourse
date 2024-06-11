5eebc4fb919ae11af8c697560095b7b3474af979
VladSalazar01 <imizurukamukura@gmail.com>
2024-06-11 17:11:49 -0500

    Exer 3.1-3.6

---
+++ part3/phonebookapi/index.js
@@ -0,0 +1,98 @@
+const http = require('http')
+const express = require('express')
+const fs = require('fs')
+const app = express()
+
+app.use(express.json())
+
+  app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+  const getPersons = () => {
+  const data = fs.readFileSync('persons.json', 'utf-8')
+  return JSON.parse(data)
+  }
+
+  const savePersons = (persons) => {
+    fs.writeFileSync('persons.json', JSON.stringify(persons, null, 2))
+  }
+
+  app.get('/api/persons', (request, response) => {
+    const persons = getPersons()
+    response.json(persons)
+  })
+
+  app.get('/api/persons/:id', (request, response) => {
+    const persons = getPersons()
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+  
+    if (person) {
+      response.json(person)
+    } else {
+        response.status(404).send('No se ha encontrado en contacto')
+    }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    let persons = getPersons()
+
+    // Filtrar la lista de personas para excluir la entrada con el ID proporcionado
+    const filteredPersons = persons.filter(person => person.id !== id)
+
+    // Verificar si se eliminó alguna entrada
+    if (persons.length !== filteredPersons.length) {
+        // Guardar la nueva lista de personas en el archivo JSON
+        savePersons(filteredPersons)
+        response.status(204).end() // Indicar que la operación se realizó con éxito (No Content)
+    } else {
+        // Si no se encuentra ninguna entrada con el ID dado, devolver un error 404
+        response.status(404).json({ error: 'Entry not found' })
+    }
+  })  
+  
+  const generateId = (persons) => {
+    const maxId = persons.length > 0
+      ? Math.max(...persons.map(n => n.id))
+      : 0
+    return maxId + 1
+  }
+  
+  app.post('/api/persons', (request, response) => {
+    const body = request.body  
+    // Verificar si falta el nombre o el número en la solicitud
+    if (!body.name || !body.number) {
+      return response.status(400).json({ 
+        error: 'Name or number is missing' 
+      })
+    }  
+    // Verificar si el nombre ya existe en el directorio telefónico
+    const persons = getPersons()
+    const duplicateName = persons.find(person => person.name === body.name)
+    if (duplicateName) {
+      return response.status(400).json({ 
+        error: 'Name already exists in the phonebook' 
+      })
+    }  
+    // Generar un nuevo ID único
+    const id = Math.floor(Math.random() * 1000000)  
+    // Crear un nuevo objeto de persona
+    const newPerson = {
+      id: id,
+      name: body.name,
+      number: body.number
+    }  
+    // Agregar la nueva persona a la lista
+    persons.push(newPerson)  
+    // Guardar la lista actualizada en el archivo JSON
+    savePersons(persons)  
+    // Responder con la nueva persona creada
+    response.json(newPerson)
+  })
+    
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
