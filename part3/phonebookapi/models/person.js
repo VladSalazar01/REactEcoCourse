3972afbfa211c4d608180d04246e31ecb8691dba
VladSalazar01 <imizurukamukura@gmail.com>
2024-07-25 19:59:25 -0500

    switched to primary

--- part3/phonebookapi/models/person.js
+++ part3/phonebookapi/models/person.js
@@ -10,7 +10,7 @@ personSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString();
     delete returnedObject._id;
     delete returnedObject.__v;
-  }
+  }   
 });
 
 module.exports = mongoose.model('Person', personSchema);
